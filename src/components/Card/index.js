fef8eff4290c5a73cb6e11119a884d57bafda785
juliandiazok <juliandiazok@hotmail.com>
2022-08-12 16:32:33 -0300

    min changes

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -178,8 +178,8 @@ export default function Card({
                 Habilidades Requeridas
               </Text>
               <View style={styles.buttonsContainer}>
-                {requiredAbilities.map(ability => (
-                  <AptitudeOffer title={ability} key={ability.id} />
+                {requiredAbilities.map(required => (
+                  <AptitudeOffer title={required} key={required.id} />
                 ))}
               </View>
               <Text
@@ -187,8 +187,8 @@ export default function Card({
                 Habilidades Secundarias
               </Text>
               <View style={styles.buttonsContainer}>
-                {desiredAbilities.map(ability => (
-                  <AptitudeOffer title={ability} key={ability.id} />
+                {desiredAbilities.map(desired => (
+                  <AptitudeOffer title={desired} key={desired.id} />
                 ))}
               </View>
               <View style={styles.botoncito}>
