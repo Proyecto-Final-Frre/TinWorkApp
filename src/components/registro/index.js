c805e280c53aa1d7b40f9f2b60fda224b81ef043
juliandiazok <juliandiazok@hotmail.com>
2022-09-25 17:14:20 -0300

    registro incompleto con img

--- src/components/registro/index.js
+++ src/components/registro/index.js
@@ -1,8 +1,27 @@
+import { Input } from '@rneui/base';
 import React from 'react';
-import {Text} from 'react-native';
+import {Image, StyleSheet, Text, View} from 'react-native';
 
-const Registro = () => {
-  return <>{<Text>LOGIN</Text>}</>;
-};
+export default function Registro() {
+  return (
+    <>
+      <View style={styles.image}>
+      <Image
+        source={require("../../images/tinwork-logo.png")}
+        resizeMode="contain"
+        style={styles.image} />
+      </View>
+      <View style={styles.form}>
+        <Input />
+      </View>
+    </>
+)
+}
 
-export default Registro;
+const styles = StyleSheet.create({
+  image : {
+    height: 150,
+    width: "100%",
+    marginBottom: 20
+  }
+})
