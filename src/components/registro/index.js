4c90f2e095b279d33963f614b02c58a5ef4efc67
juliandiazok <juliandiazok@hotmail.com>
2022-09-30 19:35:51 -0300

    Registro creado sin firebase

--- src/components/registro/index.js
+++ src/components/registro/index.js
@@ -1,15 +1,97 @@
 import {Button, Input} from '@rneui/base';
 import React, {useState} from 'react';
 import {Image, StyleSheet, Text, TextInput, View} from 'react-native';
-import {colors} from '../../constants/colors';
 import Icon from 'react-native-vector-icons/AntDesign';
+import {size} from 'lodash';
+
+import {validateEmail} from '../../utils/helpers';
+import {colors} from '../../constants/colors';
 
 export default function Registro() {
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState(defaultFormValues());
+  const [errorNombre, setErrorNombre] = useState('');
+  const [errorApellido, setErrorApellido] = useState('');
+  const [errorCorreo, setErrorCorreo] = useState('');
+  const [errorContrasena, setErrorContrasena] = useState('');
+  const [errorConfirm, setErrorConfirm] = useState('');
+
+  var numeros = '0123456789';
 
   const onChange = (e, type) => {
     setFormData({...formData, [type]: e.nativeEvent.text});
+  };
+
+  const registerUser = () => {
+    if (!validateData()) {
+      return;
+    }
+    console.log('Capoooo');
+  };
+
+  const tiene_numeros = texto => {
+    for (let i = 0; i < texto.length; i++) {
+      if (numeros.indexOf(texto.charAt(i), 0) != -1) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const validateData = () => {
+    setErrorNombre('');
+    setErrorApellido('');
+    setErrorCorreo('');
+    setErrorContrasena('');
+    setErrorConfirm('');
+    let isValid = true;
+
+    if (!validateEmail(formData.correo)) {
+      setErrorCorreo('Debes ingresar un correo válido');
+      isValid = false;
+    }
+
+    if (size(formData.nombre) < 3) {
+      setErrorNombre('Debes ingresar un nombre de al menos 3 caracteres');
+      isValid = false;
+    }
+
+    if (tiene_numeros(formData.nombre)) {
+      setErrorNombre('Debes ingresar un nombre válido');
+      isValid = false;
+    }
+
+    if (size(formData.apellido) < 3) {
+      setErrorApellido('Debes ingresar un apellido de al menos 3 caracteres');
+      isValid = false;
+    }
+
+    if (tiene_numeros(formData.apellido)) {
+      setErrorApellido('Debes ingresar un apellido válido');
+      isValid = false;
+    }
+
+    if (size(formData.password) < 6) {
+      setErrorContrasena(
+        'Debes ingresar una contraseña de al menos 6 caracteres',
+      );
+      isValid = false;
+    }
+
+    if (size(formData.confirm < 6)) {
+      setErrorConfirm(
+        'Debes ingresar una confirmacion de contraseña de al menos 6 caracteres',
+      );
+      isValid = false;
+    }
+
+    if (formData.confirm !== formData.password) {
+      setErrorContrasena('La contraseña y la confirmación deben ser iguales');
+      setErrorConfirm('La contraseña y la confirmación deben ser iguales');
+      isValid = false;
+    }
+
+    return isValid;
   };
 
   return (
@@ -26,17 +108,23 @@ export default function Registro() {
           placeholder="Nombre"
           containerStyle={styles.input}
           onChange={e => onChange(e, 'nombre')}
+          errorMessage={errorNombre}
+          defaultValue={formData.nombre}
         />
         <Input
           placeholder="Apellido"
           containerStyle={styles.input}
           onChange={e => onChange(e, 'apellido')}
+          errorMessage={errorApellido}
+          defaultValue={formData.apellido}
         />
         <Input
           placeholder="Correo"
           containerStyle={styles.input}
           onChange={e => onChange(e, 'correo')}
           keyboardType="email-address"
+          errorMessage={errorCorreo}
+          defaultValue={formData.correo}
         />
         <Input
           placeholder="Contraseña"
@@ -52,6 +140,8 @@ export default function Registro() {
               onPress={() => setShowPassword(!showPassword)}
             />
           }
+          errorMessage={errorContrasena}
+          defaultValue={formData.password}
         />
         <Input
           placeholder="Repetir contraseña"
@@ -67,12 +157,14 @@ export default function Registro() {
               onPress={() => setShowPassword(!showPassword)}
             />
           }
+          errorMessage={errorConfirm}
+          defaultValue={formData.confirm}
         />
         <Button
           title={'Registrate'}
           containerStyle={styles.btnContainer}
           buttonStyle={styles.btn}
-          onPress={() => console.log(formData)}
+          onPress={() => registerUser()}
         />
       </View>
       <View style={styles.login}>
