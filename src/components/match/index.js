ede46c441b16854c352cfa8f35d421137af47d43
juliandiazok <juliandiazok@hotmail.com>
2022-10-09 17:23:13 -0300

    pantalla completa

--- src/components/match/index.js
+++ src/components/match/index.js
@@ -1,42 +1,47 @@
-import {Animated, Image, Text, View} from 'react-native';
+import {Image, Text, View} from 'react-native';
 import {test} from '../../utils/matchs';
 import {styles} from './styles';
-import React, {useCallback, useEffect, useState} from 'react';
+import React from 'react';
 import Icon from 'react-native-vector-icons/Entypo';
 import {Card} from '@rneui/themed';
 
 const Match = () => {
   return (
     <View>
+      <Text style={styles.mismatchs}>Mis Matchs</Text>
       {test.map(offer => (
-        <Card style={styles.container}>
-          <View key={offer.id} style={styles.card}>
+        <Card>
+          <View style={styles.containerrrr}>
             <Image
-              style={[styles.img, {width: 45, height: 45, margin: 2}]}
+              style={styles.img}
               source={require('../../images/prueba-img.jpg')}
             />
-            <Text
-              style={[styles.title, {paddingBottom: '2%', paddingTop: '5%'}]}>
-              {offer.titulo}
-            </Text>
-            <View
-              style={{
-                display: 'flex',
-                flexDirection: 'row',
-              }}>
-              <Icon name={'location-pin'} size={17} />
-              <Text style={{flexGrow: 5}}>{offer.ubicacion}</Text>
-              <Icon name={'briefcase'} size={17} />
-              <Text style={{flexGrow: 3, marginLeft: 1}}> {offer.jornada}</Text>
-            </View>
-            <View
-              style={{
-                display: 'flex',
-                flexDirection: 'row',
-                paddingBottom: '5%',
-              }}>
-              <Icon name={'calendar'} size={17} />
-              <Text style={{flexGrow: 5, marginLeft: 1}}> {offer.tiempo}</Text>
+            <View style={styles.vistaGeneral}>
+              <Text style={styles.title}>{offer.titulo}</Text>
+              <View style={styles.descripcion}>
+                <View style={styles.ubicacion}>
+                  <Icon name={'location-pin'} size={17} />
+                  <Text style={styles.texto}>{offer.ubicacion}</Text>
+                </View>
+                <View style={styles.ubicacion}>
+                  <Icon name={'briefcase'} size={17} />
+                  <Text style={styles.texto}> {offer.jornada}</Text>
+                </View>
+                <View style={styles.ubicacion}>
+                  <Icon name={'calendar'} size={17} />
+                  <Text style={styles.texto}> {offer.tiempo}</Text>
+                </View>
+                {/*<View>
+                  <Icon name={'location-pin'} size={17} />
+                  <Text>{offer.ubicacion}</Text>
+                  <Icon name={'briefcase'} size={17} />
+                  <Text> {offer.jornada}</Text>
+                </View>
+                <View>
+                  <Icon name={'calendar'} size={17} />
+                  <Text> {offer.tiempo}</Text>
+      </View>*/}
+              </View>
             </View>
           </View>
         </Card>
--- src/components/match/styles.js
+++ src/components/match/styles.js
@@ -1,20 +1,43 @@
-import {CurrentRenderContext} from '@react-navigation/native';
 import {StyleSheet} from 'react-native';
-import {colors} from '../../constants/colors';
-import {CARD, FONT_SIZE, FUENTES, fuentes} from '../../utils/constants';
-import Icon from 'react-native-vector-icons/Entypo';
+import {FONT_SIZE, FUENTES, fuentes} from '../../utils/constants';
 
 export const styles = StyleSheet.create({
-  container: {
+  containerrrr: {
     display: 'flex',
     flexDirection: 'row',
-    flexWrap: 'nowrap',
-    justifyContent: 'flex-start',
     alignItems: 'center',
   },
   title: {
     fontFamily: FUENTES.BOLD,
     fontSize: FONT_SIZE.xl2,
     color: '#000',
+  },
+  vistaGeneral: {display: 'flex'},
+  descripcion: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  img: {
+    height: 45,
+    width: 45,
+    marginHorizontal: 5,
+  },
+  ubicacion: {
+    marginTop: 4,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginHorizontal: 5,
+  },
+  texto: {
+    marginHorizontal: 5,
+  },
+  mismatchs: {
+    fontSize: 35,
+    color: '#000',
+    marginHorizontal: 15,
+    marginTop: 15,
+    marginBottom: 5,
   },
 });
