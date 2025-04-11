9309ee4e82af4e858b62bb4c18207329dcc7d5af
Luciano Lugo <lucianonahuel4528@gmail.com>
2025-04-11 07:58:13 -0300

    Mejora de estilos en profile

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {ScrollView} from 'react-native';
 import Profile from '../../components/profile';
+import { BACKGROUND } from '../../utils/constants';
 
 const Profiles = ({navigation}) => {
   return (
-    <ScrollView style={{flex: 1}}>
+    <ScrollView style={{flex: 1,backgroundColor:BACKGROUND.secondary}}>
       <Profile navigation={navigation} />
     </ScrollView>
   );
