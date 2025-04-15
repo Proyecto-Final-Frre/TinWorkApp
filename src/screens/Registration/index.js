a47f1203d098801a2d609181cef1dad5a4c22945
Luciano Lugo <lucianonahuel4528@gmail.com>
2025-04-15 17:25:59 -0300

    Agrego bacground similar a web para screen de registro de usuario

--- src/screens/Registration/index.js
+++ src/screens/Registration/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import {ScrollView} from 'react-native';
 import Registro from '../../components/registro';
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
+import { BACKGROUND } from '../../utils/constants';
 
 const Registration = ({navigation}) => {
   return (
-    <KeyboardAwareScrollView>
+    <KeyboardAwareScrollView style={{backgroundColor:BACKGROUND.secondary}} >
       <Registro navigation={navigation} />
     </KeyboardAwareScrollView>
   );
