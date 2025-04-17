b2ffc6170b63bdbed944ee8884269ccf60e935c0
Luciano Lugo <lucianonahuel4528@gmail.com>
2025-04-17 20:13:39 -0300

    Subo version de react native a la 0.70

--- android/app/src/main/jni/MainApplicationModuleProvider.cpp
+++ android/app/src/main/jni/MainApplicationModuleProvider.cpp
@@ -6,7 +6,7 @@ namespace facebook {
 namespace react {
 
 std::shared_ptr<TurboModule> MainApplicationModuleProvider(
-    const std::string moduleName,
+    const std::string &moduleName,
     const JavaTurboModule::InitParams &params) {
   // Here you can provide your own module provider for TurboModules coming from
   // either your application or from external libraries. The approach to follow
@@ -18,6 +18,12 @@ std::shared_ptr<TurboModule> MainApplicationModuleProvider(
   // }
   // return rncore_ModuleProvider(moduleName, params);
   return rncore_ModuleProvider(moduleName, params);
+  // Module providers autolinked by RN CLI
+  auto rncli_module = rncli_ModuleProvider(moduleName, params);
+  if (rncli_module != nullptr) {
+    return rncli_module;
+  }
+ 
 }
 
 } // namespace react
