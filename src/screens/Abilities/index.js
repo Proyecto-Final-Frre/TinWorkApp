9c9a5f05b1010ed232f0ba777da9a586e1a142fc
Luciano Lugo <lucianonahuel4528@gmail.com>
2025-04-17 20:04:33 -0300

    Agrego fondo web en abilities

--- src/screens/Abilities/index.js
+++ src/screens/Abilities/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {ScrollView} from 'react-native';
 import {Form} from '../../components';
+import { BACKGROUND } from '../../utils/constants';
 
 const Aptitudes = ({navigation}) => {
   return (
-    <ScrollView>
+    <ScrollView style={{backgroundColor:BACKGROUND.secondary}}> 
       <Form navigation={navigation} />
     </ScrollView>
   );
